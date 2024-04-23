bab2728d76eb3234e907acca2d019bbffa03c7aa
Benhin <benhinmwendwa7@gmail.com>
2024-04-23 15:09:12 +0300

    react

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -4,7 +4,7 @@ import React from "react";
 
 import Header from "../components/Header";
 
-test("displays the text 'hello from the Header!'", () => {
+test("Now", () => {
   render(<Header />);
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
